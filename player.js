b46af44d5bfb3a33495159ef2d27820180eb3216
Sam Blundell <samcalblundell@googlemail.com>
2022-07-31 01:35:00 +0100

    create initial player movement logic

--- player.js
+++ player.js
@@ -5,14 +5,41 @@ export default class Player {
         this.height = 91.3;
         this.x = 0;
         this.y = game.height - this.height;
+        this.hVelocity = 0;
+        this.vVelocity = 0;
+        this.weight = 1;
         this.image = document.getElementById('player');
+        this.maxSpeed = 8;
+        this.jumpPower = 28;
     }
-    update() {
+    update(input) {
+        // horizontal movement
+        this.x += this.hVelocity;
+        if (input.includes('ArrowRight')) {
+            this.hVelocity = this.maxSpeed;
+        } else if (input.includes('ArrowLeft')) {
+            this.hVelocity = -this.maxSpeed;
+        } else {
+            this.hVelocity = 0;
+        }
+        if (this.x < 0) this.x = 0;
+        if (this.x > (this.game.width - this.width)) this.x = this.game.width - this.width;
+        // vertical movement
+        if (input.includes('ArrowUp') && this.onGround()) {
+            this.vVelocity = -this.jumpPower;
+        }
+        this.y += this.vVelocity;
+        if (!this.onGround()) {
+            this.vVelocity += this.weight;
+        } else {
+            this.vVelocity = 0;
+        }
 
     }
     draw(context) {
-        // context.fillStyle = 'darkred';
-        // context.fillRect(this.x, this.y, this.width, this.height);
         context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+    }
+    onGround() {
+        return this.y >= this.game.height - this.height;
     }
 }
